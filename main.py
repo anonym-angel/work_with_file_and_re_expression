6181f539de34501307d0a579840e69ffb4491688
anonym-angel <anonym-angel@yandex.ru>
2018-06-05 01:26:58 +0300

    solves the second task of 'parsing a site with movies'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import re
-
+import shutil
+from bs4 import BeautifulSoup
 
 # -------------------------- Задание номер 1 --------------------------
 
@@ -21,17 +22,38 @@ def regular_expression():
 
 # -------------------------- Задание номер 2 --------------------------
 
+def cycle_parsing():
+    count = 1
+    while count <= 3:
+        print('Страница номер {}'.format(count))
 
+        def get_html():
+            url = 'http://baskino.me/new/page/' + str(count)
+            page = requests.get(url).text
+            soup = BeautifulSoup(page, "html.parser")
+            divs = soup.find_all('div',  {'class': 'shortpost'})
+            return divs
 
+        def find_content():
+            for div in get_html():
+                div_title = div.find('div', {'class': 'posttitle'})
+                link_text = div_title.find('a').text
+                print(link_text)
 
+                img = div.find('div', {'class': 'postcover'})
+                img_src = img.find('img').get('src')
+                src = img_src
+                response = requests.get(src, stream=True)
+                with open('img/' + link_text + '.jpg', 'wb') as out_file:
+                    shutil.copyfileobj(response.raw, out_file)
 
-
-
-
-
-
-
+        find_content()
+        count += 1
 
 
 if __name__ == '__main__':
+    print('Первое задание выполнено:')
     regular_expression()
+
+    print('\nВторое задание выполнено:')
+    cycle_parsing()
